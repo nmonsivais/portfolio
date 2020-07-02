475e7c910c51b195bb2af80b0a49c0902d23a9ea
Tro <noe.monsivais@gmail.com>
2020-07-02 14:21:18 -0500

    Made header a component.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
 import React from 'react';
 import Portfolio from "./components/portfoliosection";
 import Navbar from "./components/navbar"
+import Header from "./components/header"
 
 function App() {
   return (
     <div>
 
       <Navbar />
-
-      <header class="masthead bg-primary text-white text-center">
-        <div class="container d-flex align-items-center flex-column">
-          {/* <!-- Masthead Avatar Image--> */}
-          <img class="masthead-avatar mb-5" src="assets/img/trocode.png" alt="" />
-          {/* <!-- Masthead Head</img>ing--> */}
-          <h1 class="masthead-heading mb-0">Noe &quot;Tro&quot; Monsivais</h1>
-          {/* <!-- Icon Divider--> */}
-          <div class="divider-custom divider-light">
-            <div class="divider-custom-line"></div>
-            <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
-            <div class="divider-custom-line"></div>
-          </div>
-          {/* <!-- Masthead Subheading--> */}
-          <p class="pre-wrap masthead-subheading font-weight-light mb-0">Educator - Web Designer - Video Editor</p>
-        </div>
-      </header>
+      <Header />
       <Portfolio />
 
       <section class="page-section bg-primary text-white mb-0" id="about">
