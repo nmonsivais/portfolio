86c85c09bf8204bdcaa181714f0878944baf7831
Tro <noe.monsivais@gmail.com>
2020-07-02 21:42:52 -0500

    Updates

---
+++ precache-manifest.4409e1429abe4fcd972a47859b878ad1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "112d410c2f4e7107d4917a3c599b3f42",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f6757e1846d065d5a75c",
+    "url": "/portfolio/static/css/main.277c1395.chunk.css"
+  },
+  {
+    "revision": "97e63636bc8fb388b7b4",
+    "url": "/portfolio/static/js/2.ca42dc3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.ca42dc3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6757e1846d065d5a75c",
+    "url": "/portfolio/static/js/main.18d88b8d.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
